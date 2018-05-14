60ae786e6df0844562e21388ae49d2be3d939c62
Scar <770680382@qq.com>
2018-05-14 16:00:34 +0800

    Revert "Revert "v1.1""
    This reverts commit d43898ca082464265be29ad09077d33b55e68ddb.

---
+++ Gene_decode.c
@@ -0,0 +1,79 @@
+#include"jobhead.h"
+void Gene_decode()
+{
+    int TimeTotal[chronum]; //各组总时间
+    int maxTime=0;   //当前组总时间
+    int TimeMachine[M+1];//机器最后时间
+    int TimeTool[N+1];//工件最后时间
+    int TimesTool[N+1];//工件次数
+    int i,j,k;
+    int timeStart;
+    int temp;
+    int ProcedureTotal=0;
+    for(i=1;i<=N;i++)
+    ProcedureTotal+=n_num[i];
+
+    memset(TimeMachine,0,sizeof(TimeMachine));
+    memset(TimeTool,0,sizeof(TimeTool));
+    memset(TimesTool,0,sizeof(TimesTool));
+
+	for(i=1;i<=chronum-1;i++)
+    {
+        for(j=1;j<=ProcedureTotal;j++)
+        {
+            temp=Chro[i][j];
+           // for(k=TimesTool[temp];!(Ti[temp][k]);k++)
+           // ;
+        	k=++TimesTool[temp];
+
+//			if(k>n_num[temp])
+//        	k=n_num[temp];
+
+//
+//            timeStart=(TimeTool[temp]>TimeMachine[Ma[temp][k]])?TimeTool[temp]:TimeMachine[Ma[temp][k]];
+//            TimeTool[temp]=TimeMachine[Ma[temp][k]]=timeStart+Ti[temp][k];
+//            maxTime=(maxTime>TimeTool[temp])?maxTime:TimeTool[temp];
+//
+//		printf("temp:%d\tTimesTool:%d\tMa:%d\tTime:%d\n",temp,TimesTool[temp],Ma[temp][k],Ti[temp][k]);
+		 	timeStart=Max(TimeTool[temp],TimeMachine[Ma[temp][k]]);
+            TimeTool[temp]=TimeMachine[Ma[temp][k]]=timeStart+Ti[temp][k];
+            maxTime=Max(maxTime,TimeTool[temp]);
+
+		 }
+        TimeTotal[i]=maxTime;
+//		maxTime=0;
+//		printf("%d\t%d\n",TimeTotal[i],maxTime);
+		printf("%d\n",TimeTotal[i]);
+		memset(TimesTool,0,sizeof(TimesTool));
+		memset(TimeTool,0,sizeof(TimeTool));
+    	memset(TimeMachine,0,sizeof(TimeMachine));
+		maxTime = 0 ;
+	}
+
+	int *ptr=(int *)malloc(sizeof(int)*chronum);
+	int *pptr=(int *)malloc(sizeof(int)*chronum);
+	int a,b;
+	memset(ptr,0,sizeof(int)*chronum);
+	memset(pptr,0,sizeof(int)*chronum);
+	for(a=1;a<=chronum-1;a++)
+	{//	printf("TIMETOTAL%d\n",TimeTotal[a]);
+		for(b=1;ptr[b]!=0&&TimeTotal[a]!=ptr[b]&&b<=chronum-1;b++);
+		if(TimeTotal[a]==ptr[b])
+	{
+		pptr[b]++;
+	//	printf("GG      %dTIMETOTAL%d\n",TimeTotal[a],b);
+	}
+		else {
+
+		ptr[b]=TimeTotal[a];
+		pptr[b]++;
+		}
+	}
+	for(a=1;ptr[a]!=0&&pptr[a]!=0;a++)
+	printf("Time:%d\tTimes:%d\t%d\n",ptr[a],pptr[a],a);
+
+	free(pptr);
+	free(ptr);
+
+
+}
---
+++ Others.c
@@ -0,0 +1,6 @@
+#include"jobhead.h"
+int Max(int a,int b)
+{
+	if(a>b)return a;
+	else return b;
+}
