be8f4e72fe528e972747708ac331e3488e983e13
Scar <770680382@qq.com>
2018-05-25 11:29:40 +0800

    v1.9.1

--- Codes/Gene_analysis.c
+++ Codes/Gene_analysis.c
@@ -71,47 +71,81 @@ int decode(int* NormalGeneChain)
     int *TimeTool=(int*)malloc(sizeof(int)*(N+1));
     int *TimesTool=(int*)malloc(sizeof(int)*(N+1));
 	int *TimeMachine=(int*)malloc(sizeof(int)*(M+1));
-
-    int timeStart;
+	int timeStart=-1;
     int temp;
     int maxTime=0;
     int i,j,k;
+    int c,d;
     memset(TimeMachine,0,sizeof(int)*(M+1));
     memset(TimeTool,0,sizeof(int)*(N+1));
     memset(TimesTool,0,sizeof(int)*(N+1));
+    int MaTimeStart[M+1][N+1];
+    int MaTimeStop[M+1][N+1];
+	for(i=0;i<=M;i++)
+	{	
+    memset(MaTimeStart[i],0,sizeof(int)*(N+1));
+    memset(MaTimeStop[i],0,sizeof(int)*(N+1));
+	}
+    int ma,a;
+	for(j=1;j<=ProcedureTotal;j++)
+	{	
+		temp=NormalGeneChain[j];
+   		k=++TimesTool[temp];
+		ma=Ma[temp][k];
 
-    for(j=1;j<=ProcedureTotal;j++)
-    {
-        temp=NormalGeneChain[j];
-       	k=++TimesTool[temp];
-	 	timeStart= Max(TimeTool[temp],TimeMachine[Ma[temp][k]]);
-		TimeTool[temp]=TimeMachine[Ma[temp][k]]=timeStart+Ti[temp][k];
-        maxTime=Max(maxTime,TimeTool[temp]);
-    }
+	   	for(a=1;MaTimeStop[ma][a]!=0&&timeStart==-1;a++)
+	   		{if(MaTimeStart[ma][a+1]-MaTimeStop[ma][a]>=Ti[temp][k]&&(MaTimeStop[ma][a]>=TimeTool[temp]||MaTimeStart[ma][a+1]-Ti[temp][k]>=TimeTool[temp]))
+	   			timeStart=MaTimeStop[ma][a];
+	   		}		
+	   	if(timeStart==-1)
+ 		{	
+		 	for(a=1;MaTimeStop[ma][a]!=0;a++)
+		 	;
+			timeStart=Max(TimeTool[temp],TimeMachine[ma]);
+			MaTimeStart[ma][a]=timeStart;
+ 			TimeMachine[ma]=TimeTool[temp]=MaTimeStop[ma][a]=timeStart+Ti[temp][k];
+ 			maxTime=Max(maxTime,MaTimeStop[temp][a]);
+ 		}
+ 		else if(MaTimeStop[ma][a]>=TimeTool[temp]) 
+ 		{
+ 			MaTimeStop[ma][a]+=Ti[temp][k];
+ 			TimeTool[temp]=MaTimeStop[ma][a];
+ 		}
+ 		else 
+ 		{	
+ 			TimeTool[temp]=MaTimeStart[ma][a+1];
+ 			MaTimeStart[ma][a+1]-=Ti[temp][k];
+		 }
+    	timeStart=-1;
+	}
+	free(TimeMachine);
+	free(TimesTool);
+	free(TimeTool);
+
+	for(c=1;c<=M;c++)
+	for(d=1;MaTimeStop[c][d]!=0;d++)
+	maxTime=Max(maxTime,MaTimeStop[c][d]);
+			
 	if(GreatGeneNum==0)
-	{
+	{	
+		printf("%d to %d",GreatGeneNum,maxTime);
 			GreatGeneNum=maxTime;
 		for(i=1;i<=ProcedureTotal;i++)
-   			GreatGeneChain[i]=NormalGeneChain[i];
-   			GreatGeneChain[0]=GreatGeneNum=maxTime;
-		}
+			GreatGeneChain[i]=NormalGeneChain[i];
+		GreatGeneChain[0]=GreatGeneNum=maxTime;
+		printf("Change in %d generations\n",generation);
+	}
 	else
 	{
 	if(GreatGeneNum>maxTime)
-	{		
+	{	printf("%d to %d",GreatGeneNum,maxTime);
    		GreatGeneNum=maxTime;
    		GreatGeneChain[0]=GreatGeneNum;
     	for(i=1;i<=ProcedureTotal;i++)
    		GreatGeneChain[i]=NormalGeneChain[i];
    		printf("Change in %d generations\n",generation);
 	}}
-//	printf("maxTime:%3dGreatGeneNum:%d\n",maxTime,GreatGeneChain[1]);
-	//if(GreatGeneNum==0)
-	//printf("dd\n");
 	NormalGeneChain[0]=maxTime;
-	free(TimeMachine);
-	free(TimesTool);
-	free(TimeTool);
 	return maxTime;
 }
 
